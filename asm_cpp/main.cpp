3848a18192248c3cb5affdd9a49690bd273afd23
artemis <marlkiller@voidm.com>
2022-06-23 15:38:40 +0800

    Initial Commit

---
+++ asm_cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  asm_cpp
+//
+//  Created by artemis on 2022/6/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
