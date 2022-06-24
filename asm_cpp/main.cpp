5b170bf189ede5f10d9cca2ee101063e193f0b67
artemis <marlkiller@voidm.com>
2022-06-24 16:40:45 +0800

    update

--- asm_cpp/main.cpp
+++ asm_cpp/main.cpp
@@ -10,8 +10,10 @@
 
 using namespace std;
 
+
 #if defined _WIN32
     #if defined(_M_X64) 
+//    extern "C" uintptr_t asm_win64_add(uintptr_t u1, uintptr_t u2,uintptr_t u3);
     EXTERN_C ULONG64 asm_win64_add(ULONG64 u1, ULONG64 u2,DWORD_PTR u3);
     EXTERN_C ULONG64 asm_win64_call(DWORD_PTR u1);   
     #endif
