0d0f43c3d6d029d01d5eb8e1191406b9dd2283f1
voidm <marlkiller@voidm.com>
2022-06-28 00:11:40 +0800

    update

--- include/main.h
+++ include/main.h
@@ -1,17 +1,1 @@
-
-
-#ifdef DLL_EXPORTS
-#define lib_def _declspec(dllexport)
-#else
-#define lib_def
-#endif
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-lib_def int lib_dev(int a, int b);
-
-#ifdef __cplusplus
-}
-#endif
+int lib_dev(int a, int b);
