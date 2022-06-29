77695ba99e4f6128eb5da9ecf4033565f936a1d0
artemis <marlkiller@voidm.com>
2022-06-29 13:09:19 +0800

    update include header

--- build_run.cmd
+++ build_run.cmd
@@ -2,11 +2,15 @@
 
 rmdir /s /q build
 
+set /p flag="Clean CMakeCache.txt ?  y/n :"
+if "%flag%" == "y" (
+    rmdir /s /q CMakeCache.txt
+)
+
 set /p flag="Build x64 ? If type 'N' will build with x86 !  y/n :"
 if "%flag%" == "y" (
     cmake -A x64 -B build . 
     cmake --build ./build --config Release
-
 ) else (
     cmake -A Win32 -B build . 
     cmake --build ./build --config Release
